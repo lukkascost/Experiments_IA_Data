94d6c4053858035dcedba923ca39d821044be940
lukkascost <lucas.costa@lit.ifce.edu.br>
2021-03-27 17:13:40 -0300

    Attribute module and commons project with entities.

--- attributes.module/src/main/resources/db/migration/V1_02_00__create_samples_table.sql
+++ attributes.module/src/main/resources/db/migration/V1_02_00__create_samples_table.sql
@@ -6,8 +6,11 @@ create table samples
     extractor_type varchar(100),
     label varchar(255),
     original_file_name varchar(255),
+    dataset_id uuid not null,
     constraint samples_pkey
-        primary key (id)
+        primary key (id),
+    constraint fknlgccqh3r9gxiiu18nwtdluor
+        foreign key (dataset_id) references datasets
 );
 
 alter table samples owner to postgres;
