8b72f77bc2c6a0986e052ac65c9730d63718b94e
lukkascost <lucas.costa@lit.ifce.edu.br>
2021-04-24 18:55:37 +0100

    cloud service compile arm

--- build-push-arm32.sh
+++ build-push-arm32.sh
@@ -5,9 +5,17 @@ docker manifest create lukkascost/data-analyze-commons:latest lukkascost/data-an
 docker manifest push lukkascost/data-analyze-commons:latest
 
 
-docker build -t lukkascost/api-gateway api-gateway/
+docker build -t lukkascost/api-gateway:latest-arm api-gateway/
 docker push lukkascost/api-gateway:latest-arm
 docker manifest rm lukkascost/api-gateway:latest || true
 docker manifest create lukkascost/api-gateway:latest lukkascost/api-gateway:latest-arm lukkascost/api-gateway:latest-amd64
 docker manifest push lukkascost/api-gateway:latest
+
+
+docker build -t lukkascost/cloud-discovery:latest-arm cloud-service/service-discovery/
+docker push lukkascost/cloud-discovery:latest-arm
+docker manifest rm lukkascost/cloud-discovery:latest || true
+docker manifest create lukkascost/cloud-discovery:latest lukkascost/cloud-discovery:latest-arm lukkascost/cloud-discovery:latest-amd64
+docker manifest push lukkascost/cloud-discovery:latest
+
 
