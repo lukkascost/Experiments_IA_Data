c37c9772ae0a4b2d9034036d5793e818fcba1c26
lukkascost <lucas.costa@lit.ifce.edu.br>
2022-08-17 09:37:33 -0300

    websocket

--- api-gateway/src/main/java/br/com/lukkascost/api/gateway/configurations/WebSocketConfig.java
+++ api-gateway/src/main/java/br/com/lukkascost/api/gateway/configurations/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/logger-websocket").setAllowedOrigins("*");
+        registry.addEndpoint("/").setAllowedOrigins("*");
     }
 }
