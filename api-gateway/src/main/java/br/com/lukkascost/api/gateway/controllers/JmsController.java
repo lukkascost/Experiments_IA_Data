e4c63a38e40ccbb7a6477c5ab8efe76edd6043c4
lukkascost <lucas.costa@lit.ifce.edu.br>
2022-08-17 09:22:56 -0300

    websocket on activemq

--- api-gateway/src/main/java/br/com/lukkascost/api/gateway/controllers/JmsController.java
+++ api-gateway/src/main/java/br/com/lukkascost/api/gateway/controllers/JmsController.java
@@ -1,19 +1,25 @@
 package br.com.lukkascost.api.gateway.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.jms.core.JmsTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@RequiredArgsConstructor
 public class JmsController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final ObjectMapper mapper;
 
-    public JmsController(SimpMessagingTemplate simpMessagingTemplate, JmsTemplate jmsTemplate, ObjectMapper mapper) {
-        this.simpMessagingTemplate = simpMessagingTemplate;
-        this.mapper = mapper;
+    @JmsListener(destination = "generic.events")
+    public void receivePostEventMessage(String message) throws JsonProcessingException {
+        Map<String, String> event = mapper.convertValue(message, Map.class);
+        simpMessagingTemplate.convertAndSend(event.get("subscriptionUrl"), event.get("message"));
     }
 
 }
